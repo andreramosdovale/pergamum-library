5ac635c8042649ac974c251200625034ba0e03c5
andre vale <andre_ramosdovale@outlook.com>
2023-11-24 14:25:13 -0300

    feat: create featch wrapper for requests

---
+++ src/helpers/fetchWrapper.ts
@@ -0,0 +1,43 @@
+const controller = new AbortController();
+const { signal } = controller;
+
+const defaultHeaderOptions = {
+  "Acces-Control-Allow-Origin": "*",
+};
+
+function bindUrl(path: string) {
+  return `https://pergamum-biblioteca.pucpr.br/api/consulta/${path}`;
+}
+
+async function handleResponse(response: Response | void) {
+  const data = await response?.json?.();
+
+  if (!response?.ok) {
+    const error = !data;
+    return Promise.reject(error);
+  }
+
+  return data;
+}
+
+async function get(url: string, options?: RequestInit) {
+  const requestOptions = {
+    headers: defaultHeaderOptions,
+    method: "GET",
+    signal,
+    ...options,
+  } as RequestInit;
+
+  if (signal.aborted) {
+    return;
+  }
+
+  const response = await fetch(bindUrl(url), requestOptions);
+
+  return handleResponse(response);
+}
+
+export const fetchWrapper = {
+  get,
+  abortAll: () => controller.abort(),
+};
