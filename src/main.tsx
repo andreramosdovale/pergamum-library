a0766d6afd0e0a1aa1ab94164fd65698865d7335
andre vale <andre_ramosdovale@outlook.com>
2023-11-24 13:47:57 -0300

    chore: add radix ui and sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Theme } from "@radix-ui/themes";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "@radix-ui/themes/styles.css";
+import "./styles/global.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Theme appearance="light" accentColor="purple" panelBackground="solid">
+      <App />
+    </Theme>
+  </React.StrictMode>
+);
