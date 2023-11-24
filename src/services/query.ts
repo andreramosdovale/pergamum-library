904838ef73218324f6b6cc397de84e56a83fb86e
andre vale <andre_ramosdovale@outlook.com>
2023-11-24 15:12:55 -0300

    feat: created calls for api

---
+++ src/services/query.ts
@@ -0,0 +1,25 @@
+import { bookResponse, indexOptionsResponse } from "../types/response";
+import { fetchWrapper } from "../helpers/fetchWrapper";
+
+async function books(option: string = "LIVRE", filter: string) {
+  return fetchWrapper
+    .get(
+      `?coluna_um=${option}&search_id=1&search_tab=pesquisa_geral&termo_pesquisa=${filter}`
+    )
+    .then((response: Array<bookResponse>) => {
+      return { data: response, count: response.length };
+    });
+}
+
+async function indexOptions() {
+  return fetchWrapper
+    .get(`/opcoes-indice`)
+    .then((response: Array<indexOptionsResponse>) => {
+      return { data: response, count: response.length };
+    });
+}
+
+export default {
+  books,
+  indexOptions,
+};
