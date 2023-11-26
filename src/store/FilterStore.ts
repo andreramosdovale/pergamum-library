fa80aaa97d0cfbb8efa86509991e18e912307894
andre vale <andre_ramosdovale@outlook.com>
2023-11-26 20:31:52 -0300

    fix: fix filter store

--- src/store/FilterStore.ts
+++ src/store/FilterStore.ts
@@ -1,18 +1,18 @@
-// import { create } from "zustand";
+import { create } from "zustand";
 
-// type Item = {
-//   type: "img" | "ref" | "type";
-//   term: string;
-// };
+type Item = {
+  type: "img" | "ref" | "type";
+  term: string;
+};
 
-// type FilterStore = {
-//   filter: Item;
-//   changeState: (item: Item) => void;
-// };
+type FilterStore = {
+  filter: Item;
+  changeFilter: (item: Item) => void;
+};
 
-// export const useFilterStore = create<FilterStore>((set) => {
-//   return {
-//     filter: { type: "", term: "ref" },
-//     changeState: (item) => set({ filter: item }),
-//   };
-// });
+export const useFilterStore = create<FilterStore>((set) => {
+  return {
+    filter: { type: "ref", term: "" },
+    changeFilter: (item) => set({ filter: item }),
+  };
+});
