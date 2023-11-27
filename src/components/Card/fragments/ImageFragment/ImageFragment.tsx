b173ccd4bf4124adb8b547df0f04dac51cbba46d
andre vale <andre_ramosdovale@outlook.com>
2023-11-26 23:39:11 -0300

    fix: fix images url

--- src/components/Card/fragments/ImageFragment/ImageFragment.tsx
+++ src/components/Card/fragments/ImageFragment/ImageFragment.tsx
@@ -8,19 +8,24 @@ interface ICardProps {
 export function ImageFragment({ description, image }: ICardProps) {
   return (
     <>
-      <Inset clip="padding-box" side="top" pb="current">
-        <img
-          src={image}
-          style={{
-            display: "block",
-            objectFit: "cover",
-            width: "100%",
-            height: 140,
-            backgroundColor: "var(--gray-5)",
-          }}
-        />
-      </Inset>
-      <div className="card-line" />
+      {image !== "" ? (
+        <>
+          <Inset clip="padding-box" side="top" pb="current">
+            <img
+              src={image.replace("N", "")}
+              style={{
+                display: "block",
+                objectFit: "cover",
+                width: "100%",
+                height: 140,
+                backgroundColor: "var(--gray-5)",
+              }}
+            />
+          </Inset>
+          <div className="card-line" />
+        </>
+      ) : null}
+
       <Text weight={"bold"} className="card-content-text">
         {description}
       </Text>
