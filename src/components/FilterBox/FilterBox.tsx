90aa0e86c54963d6702c37867a4d9a0a70ad520d
andre vale <andre_ramosdovale@outlook.com>
2023-11-27 00:35:55 -0300

    feat: creating export

--- src/components/FilterBox/FilterBox.tsx
+++ src/components/FilterBox/FilterBox.tsx
@@ -3,6 +3,7 @@ import {
   Box,
   Button,
   Checkbox,
+  Dialog,
   Flex,
   Select,
   Text,
@@ -18,6 +19,8 @@ export function FilterBox() {
   const [inputValue, setInputValue] = useState<string>("");
   const [, setDebouncedInputValue] = useState<string>("");
   const [selectAllSelected, setSelectAllSelected] = useState<boolean>(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   const exportStore = useExportStore();
   const filterStore = useFilterStore();
@@ -66,42 +69,95 @@ export function FilterBox() {
     exportStore.clearSelected();
   };
 
+  const sendEmail = () => {};
+
   return (
-    <Flex className="box" direction={"row"} wrap={"wrap"} gap="3">
-      <Box grow={"1"}>
-        <TextField.Input
-          size="3"
-          placeholder="Termo"
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-      </Box>
-      <div className="line" />
-      <Select.Root
-        size="3"
-        defaultValue="ref"
-        onValueChange={(value) =>
-          filterStore.changeFilter({
-            term: filterStore.filter.term,
-            type: value,
-          })
-        }
-      >
-        <Select.Trigger />
-        <Select.Content position="popper">
-          <Select.Item value="ref">Referencia</Select.Item>
-          <Select.Item value="type">Tipo de obra</Select.Item>
-          <Select.Item value="img">Capa</Select.Item>
-        </Select.Content>
-      </Select.Root>
-      <div className="line" />
-      <Button size="3" variant="classic" disabled={disable}>
-        Exportar
-      </Button>
-      <Flex align={"center"} gap={"2"}>
-        <Checkbox checked={!selectAllSelected} onClick={() => onSelect()} />
-        <Text>{!selectAllSelected ? "Remover todos" : "Selecionar todos"}</Text>
-      </Flex>
-    </Flex>
+    <>
+      <Dialog.Root>
+        <Flex className="box" direction={"row"} wrap={"wrap"} gap="3">
+          <Box grow={"1"}>
+            <TextField.Input
+              size="3"
+              placeholder="Termo"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </Box>
+          <div className="line" />
+          <Select.Root
+            size="3"
+            defaultValue="ref"
+            onValueChange={(value) =>
+              filterStore.changeFilter({
+                term: filterStore.filter.term,
+                type: value,
+              })
+            }
+          >
+            <Select.Trigger />
+            <Select.Content position="popper">
+              <Select.Item value="ref">Referencia</Select.Item>
+              <Select.Item value="type">Tipo de obra</Select.Item>
+              <Select.Item value="img">Capa</Select.Item>
+            </Select.Content>
+          </Select.Root>
+          <div className="line" />
+          <Dialog.Trigger>
+            <Button
+              size="3"
+              variant="classic"
+              disabled={disable}
+              className="box-btn"
+            >
+              Exportar
+            </Button>
+          </Dialog.Trigger>
+          <Flex align={"center"} gap={"2"}>
+            <Checkbox checked={!selectAllSelected} onClick={() => onSelect()} />
+            <Text>
+              {!selectAllSelected ? "Remover todos" : "Selecionar todos"}
+            </Text>
+          </Flex>
+        </Flex>
+        <Dialog.Content style={{ maxWidth: 450 }}>
+          <Dialog.Title>Exportar</Dialog.Title>
+          <Dialog.Description size="2" mb="4">
+            Envie os dados selecionados para seu Email.
+          </Dialog.Description>
+
+          <Flex direction="column" gap="3">
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Nome
+              </Text>
+              <TextField.Input
+                placeholder="Digite seu nome."
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+            <label>
+              <Text as="div" size="2" mb="1" weight="bold">
+                Email
+              </Text>
+              <TextField.Input
+                placeholder="Digite seu Email."
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </Flex>
+
+          <Flex gap="3" mt="4" justify="end">
+            <Dialog.Close>
+              <Button variant="soft" color="gray">
+                Cancelar
+              </Button>
+            </Dialog.Close>
+            <Dialog.Close>
+              <Button onClick={() => sendEmail()}>Enviar</Button>
+            </Dialog.Close>
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Root>
+    </>
   );
 }
